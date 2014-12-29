4b9bdb138b6fca5c4acf46e45a8921543b1153f3
Red Li <writeshadow@gmail.com>
2014-12-29 15:48:39 -0800

    add gtest

--- bench/main.cpp
+++ bench/main.cpp
@@ -12,30 +12,17 @@
 #include "ipptype.hpp"
 #include "ipps.hpp"
 
+#include "test_utils.hpp"
 #include "ipptype_test.hpp"
 
 
-class test_executor
-{
-    std::vector<std::shared_ptr<test_case>> tests_;
-public:
-
-
-    template<typename T, typename ...Args>
-    int add_test(Args ...args)
-    {
-        auto t = std::make_shared<T>(args...);
-
-        tests_.push_back(std::dynamic_pointer_cast<test_case>(t));
-    };
-
-};
 
 
 
 int main(int argc, char *argv[])
 {
     test_executor exec;
+    
 
     return 0;
 }
